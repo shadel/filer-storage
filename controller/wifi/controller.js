cc8dc0ac4d9694d51300f9bab3eee4cd74898255
Khanh Vu <khanh.vu@insightus.com.au>
2018-06-21 12:33:07 +1000

    prepare for wifi

--- controller/wifi/controller.js
+++ controller/wifi/controller.js
@@ -1,3 +1,9 @@
+const fs = require('fs');
+const async = require('async');
+const iwlist = require('wireless-tools/iwlist');
+const exec = require("child_process").exec;
+
+
 class WifiController {
   constructor() {
 
@@ -19,6 +25,31 @@ class WifiController {
       resolve(true);
     });
   }
+
+  reboot_wlan0(callback){
+    exec("sudo wpa_cli -i wlan0 reconfigure", (error,stdout,stderr)=>
+    {
+      if(!error){
+        callback();
+      }
+      else{
+        console.log("error while reset wlan0");
+      }
+    });
+  }
+  getSurroundWifi(){
+    iwlist.scan("wlan0",(err,networks)=>{
+      if(err){
+          console.log('error while scan');
+      }
+      else{
+          return networks();
+      }
+    });
+  }
+  connectWifiWithCredential(ssid,key,callback){
+  }
+
 }
 
 module.exports = WifiController;
